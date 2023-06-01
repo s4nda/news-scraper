a3428d0be1bcd81cdd066989f3972a72beee7af6
s4nda <bulatovicsanda@gmail.com>
2023-06-01 18:22:56 +0200

    refactor parser

--- scraper/new_scraper.py
+++ scraper/new_scraper.py
@@ -6,27 +6,43 @@ from config import Config
 
 
 class BaseScraper:
-    def __init__(self, url, parameters=None):
-        self.url = url
-        self.parameters = parameters
+    base_url = ""
+    url = ""
+    institution_id = ""
 
-    def get_page(self):
-        pg = requests.get(self.url, params=self.parameters)
+    def make_params(self, page, category_id=None):
+        pass
+
+    def get_page(self, page=None, category_id=None):
+        compiled_params = self.make_params(page, category_id)
+        pg = requests.get(self.url, params=compiled_params)
         html = pg.text
         return html
 
-    def parse_page(self, html):
-        soup = BeautifulSoup(html, "lxml")
+    def parse_page(self, soup):
+        raise Exception("Method not implemented")
+
+    def parse(self, start_page=0, end_page=0, category_id=None) -> list[NewsItem]:
+        """
+        Some class description , sanda
+        """
+        news_items = []
+        for i in range(start_page, end_page):
+            page = self.get_page(page=i, category_id=category_id)
+            soup = BeautifulSoup(page, "lxml")
+            news_items += self.parse_page(soup)
+            if self.parse_page == []:
+                break
+        return news_items
 
 
 class BGFilozofski(BaseScraper):
-    def __init__(
-        self, url, parameters=None, category_id=None, start_page=None, end_page=None
-    ):
-        super().__init__(url, parameters)
-        self.category_id = category_id
-        self.start_page = start_page
-        self.end_page = end_page
+    base_url = "https://www.f.bg.ac.rs"
+    url = base_url + "/vesti"
+    institution_id = 1
+
+    def make_params(self, page, category_id=None):
+        return {"IDO": category_id, "str": page}
 
     def list_categories(self):
         html = self.get_page()
@@ -47,8 +63,7 @@ class BGFilozofski(BaseScraper):
             categories.append(item)
         return categories
 
-    def parse_page(self, html, category_id) -> list[NewsItem]:
-        soup = BeautifulSoup(html, "lxml")
+    def parse_page(self, soup: BeautifulSoup) -> list[NewsItem]:
         children = soup.find_all("div", class_="vest-naslov")
         category = soup.find(id="odeljenje_vesti-naslov")
         if category:
@@ -56,8 +71,7 @@ class BGFilozofski(BaseScraper):
         news = []
         for child in children:
             item = {}
-            item["institution_id"] = Config.filozofski_id
-            item["category_id"] = category_id
+            item["institution_id"] = self.institution_id
             item["category"] = category
             parent = child.find_parent("tr")
             if parent:
@@ -82,14 +96,37 @@ class BGFilozofski(BaseScraper):
 
 
 class NSFilozofski(BaseScraper):
-    def __init__(self, url, parameters=None):
-        super().__init__(url, parameters)
-        pass
+    base_url = "https://www.ff.uns.ac.rs"
+    url = base_url + "/sr/vesti"
+    institution_id = 2
+
+    def make_params(self, page, category_id=None):
+        return {"page": page}
+
+    def parse_page(self, soup):
+        news = []
+        for element in soup.find_all("article", class_="vest-arhiva"):
+            item = {}
+            item["institution_id"] = self.institution_id
+            title = element.find(class_="vest-naslov").get_text(strip=True)
+            item["title"] = title
+            date = element.find("time").get_text().replace(".", "")
+            item["date"] = date
+            body = element.find(class_="vest-ukratko").get_text()
+            item["body"] = body
+            a = element.find("a")
+            href = a.attrs.get("href")
+            attachment = self.base_url + href
+            item["attachment"] = attachment
+            news_item = NewsItem(**item)
+            news.append(news_item)
+        return news
 
 
-bg = BGFilozofski("https://www.f.bg.ac.rs/vesti")
-ns = NSFilozofski("https://www.ff.uns.ac.rs/sr/vesti")
+bg = BGFilozofski()
+ns = NSFilozofski()
 
 # scraper.get_page()
 
-print(bg.list_categories())
+res = ns.parse(end_page=5, category_id=3)
+print(len(res))
--- scripts/test_script.py
+++
@@ -1,8 +0,0 @@
-if __name__ == "__main__":
-
-    def hello_world():
-        print("hello world")
-
-    hello_world()
-
-    
